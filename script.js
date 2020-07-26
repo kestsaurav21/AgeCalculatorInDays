5a03d0291fbb3c2c921b96d1279b76cdd532ad2e
kestsaurav21 <kestwalsaurav7@gmail.com>
2020-07-27 01:53:43 +0530

    First Project

---
+++ script.js
@@ -0,0 +1,16 @@
+//Challenge 1: Your Age in Da
+
+function ageInDays() {
+    var birthYear = prompt("What is your BirthYear ?");
+    var ageInDayss = (2020 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    
+}
+
+function reset(){
+     document.getElementById('ageInDays').remove();
+}
